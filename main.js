6a5b6bbcbcad9e312b6d0f510d9738c4706f1c36
GriseldaZBalmaceda <gzbalmaceda@my365.bellevue.edu>
2019-04-11 20:26:40 -0500

    adding views

--- main.js
+++ main.js
@@ -1,11 +1,7 @@
 var quizModelView = function () {
-    //index is an observable since we will want to display different questions 
-    this.index = ko.observable(0);
-    //the correct Answers array will hold our users correct answers. Or should we just have an array with our Users answers and then in the end just compare
-    //the array with the quiz answers?
-    this.correctAnswers = ko.observableArray([]);
-    this.quiz = [
-        //using the constructor I went ahead and populated the quiz array with our questions
+    self = this;
+    self.index = ko.observable(0);
+    self.quiz = [
         new quizViewModel("In JavaScript you cannot use what reserved words as variables, labels, or function names?", ["Reserved Words", "Strings", "Data Types", "Number"], "Reserved Words"),
         new quizViewModel("Which of the following is used for declaring variables in a function, but is block scoped?", ["Null", "var", "const", "let"], "let"),
         new quizViewModel("A variable declared outside a JavaScript function, becomes what?", ["Local", "Global", "Invalid", "Number"], "Global"),
@@ -17,45 +13,116 @@ var quizModelView = function () {
         new quizViewModel("Which of the following is used to add an HTML element using JavaScript?", ["document.createElement(element)", "document.appendChild(element)", "document.write(text)", "document.replaceChild(new, old)"], "document.appendChild(element)"),
         new quizViewModel("In JavaScript, which of the following is used to comment lines:", ["/* and */", "//", "Data Types", "<!-- and  -->"], "<!-- and  -->"),
     ];
-//we will start witht the first question inside the quiz array
-    this.currentQuestion = ko.observable(this.quiz[0]);
+    self.guesses = ko.observableArray([]);
+    self.correct = ko.observable(0)
+    self.click = ko.observable(0)
 
-    //created this currentAnswer so maybe we can compare the users answers with the current question answer and then push true or false in the correct answers array?
-    this.currentAnswer = ko.observable(this.currentQuestion.answers)
- 
-//this function takes the current index and adds 1. We then sset the current question with the new index 
-    this.nextQuestion = function () {
-        console.log('next')
-        this.index(this.index() + 1)
-        this.currentQuestion(this.quiz[this.index()])
-    }
-//this function is the same as  nextQuestion but it removes an index to display previous question
-    this.previousQuestion = function () {
-        console.log('previous')
-        this.index(this.index() - 1)
-        this.currentQuestion(this.quiz[this.index()])
-    }
-    //created this function so we can display question that is selected via the li group.
-     this.showQuestion = function (index) {
-        this.currentQuestion(this.quiz[index])
-    }
-    //started this function to check the answers but I'm doing something wrong...thoughts?
-    this.checkAnswer=function(guess){
-    console.log(guess)
-    if(guess===this.currentQuestion().answers){
-        this.correctAnswers.push(guess)
-        console.log('correct')
+    self.currentQuestion = ko.observable(self.quiz[0]);
+    self.currentAnswer = ko.observable(self.quiz[0].answer)
+
+    self.checkAnswer = function (guess) {
+        if (guess === self.currentAnswer()) {
+            self.correct(self.correct() + 1)
+            console.log('correct')
+        } else {
+            console.log('false')
+        }
+
     }
 
+    self.nextQuestion = function () {
+        self.currentQuestion(self.quiz[self.index()])
+        self.currentAnswer(self.quiz[self.index()].answer)
+        self.index(self.index() + 1)
+    }
+
+    self.previousQuestion = function () {
+
+        self.currentQuestion(self.quiz[self.index()])
+        self.currentAnswer(self.quiz[self.index()].answer)
+        self.index(self.index() - 1)
+    }
+    self.showQuestion = function (index) {
+        self.currentQuestion(self.quiz[index])
+    }
+
+    self.selected = function (choiceSelected) {
+        if (self.click() < 1) {
+
+            self.currentQuestion().choices().forEach(function (choice) {
+                choice.select(false)
+            })
+            choiceSelected.select(true)
+            self.checkAnswer(choiceSelected.choice)
+
+
+        } else if (self.click() >= 2) {
+            self.currentQuestion().choices().forEach(function (choice) {
+                choice.select(true)
+            })
+
+        }
+
+
+    }
+    console.log(self.guesses())
+}
+
+var choicesViewModel = function (choice) {
+    var self = this;
+    self.choice = choice;
+    self.select = ko.observable(true);
+
+}
+
+
+var quizViewModel = function (question, choiceOptions, answer) {
+    var self = this;
+    self.guess = ko.observable([]);
+    self.answer = answer;
+    self.click = 0
+    self.question = question;
+    self.choices = ko.observableArray([])
+    for (var i = 0; i < choiceOptions.length; i++) {
+        self.choices.push(new choicesViewModel(choiceOptions[i]));
     }
 }
 
-//created a viewModel constructor for the questions!
-var quizViewModel = function (question, choices, answers) {
-    this.question = question;
-    this.choices = choices;
-    this.answers = answers;
+
+var finalScore = function(question,checked){
+
 }
 
+
+
+//found this and thought maybe we want to try a custom binding for our dbl click 
+
+// ko.bindingHandlers.click = {
+//     init: function(element, valueAccessor, allBindingsAccessor, viewModel, context) {
+//         var accessor = valueAccessor();
+//         var clicks = 0;
+//         var timeout = 200;
+
+//         $(element).click(function(event) {
+//             if(typeof(accessor) === 'object') {
+//                 var single = accessor.single;
+//                 var double = accessor.double;
+//                 clicks++;
+//                 if (clicks === 1) {
+//                     setTimeout(function() {
+//                         if(clicks === 1) {
+//                             single.call(viewModel, context.$data, event);
+//                         } else {
+//                             double.call(viewModel, context.$data, event);
+//                         }
+//                         clicks = 0;
+//                     }, timeout);
+//                 }
+//             } else {
+//                 accessor.call(viewModel, context.$data, event);
+//             }
+//         });
+//     }
+// };
 
 ko.applyBindings(quizModelView);
